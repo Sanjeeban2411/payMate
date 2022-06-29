bee2033d64b2382fb2c0a7c43b7f59023e996d43
Sanjeeban2411 <sanju.sanjeeban.sp@gmail.com>
2022-06-29 22:32:18 +0530

    client error solved

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-// import { FaUserAlt, FaMoneyBillWave } from 'react-icons/fa'
-// import { AiOutlineClose } from 'react-icons/ai'
-// import { AiOutlineMenu } from 'react-icons/ai'
-// import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { FaUserAlt, FaMoneyBillWave } from 'react-icons/fa'
+import { AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineMenu } from 'react-icons/ai'
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 
 const Navbar = () => {
@@ -16,7 +16,7 @@ const Navbar = () => {
         <div>
             <div className='flex w-full justify-between items-center h-20 px-10 absolute z-10 font-Montserrat '>
                 <div className='text-black' >
-                    {/* <h1 onClick={handleNav} className={logo ? ' hidden' : 'flex text-3xl font-black  '} >Paymate <div className='p-2 m-auto'><FaMoneyBillWave /></div></h1> */}
+                    <h1 onClick={handleNav} className={logo ? ' hidden' : 'flex text-3xl font-black  '} >Paymate <div className='p-2 m-auto'><FaMoneyBillWave /></div></h1>
                 </div>
 
                 <ul className='hidden md:flex'>
@@ -26,12 +26,12 @@ const Navbar = () => {
                     <li className='p-7 font-bold text-black cursor-pointer'>About Us</li>
                 </ul>
                 <div className=' hidden md:flex md:pr-3 justify-between items-center  cursor-pointer'>
-                    {/* <button className='rounded-full p-3 bg-black text-white'><FaUserAlt /></button> */}
-                    {/* <h2 className='text-2xl font-bold m-2 text-red-500'>Login</h2> */}
+                    <button className='rounded-full p-3 bg-black text-white'><FaUserAlt /></button>
+                    <h2 className='text-2xl font-bold m-2 text-red-500'>Login</h2>
                 </div>
                 <div className='hidden md:h-3'><hr/></div>
                 <div className='md:hidden z-10 text-red-500' onClick={handleNav}>
-                    {/* {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />} */}
+                    {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
                 </div>
                 <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-6 flex flex-col' : 'absolute left-[-100%]'}>
                     <ul>
@@ -44,12 +44,12 @@ const Navbar = () => {
                     <div className='flex flex-col pb-4'>
                         <button className='p-3 border bg-gradient-to-r from-red-600 to-red-400 text-white rounded-md'>ACCOUNT</button>
                     </div>
-                    {/* <div className='flex justify-between my-3'>
+                    <div className='flex justify-between my-3'>
                         <FaFacebook className='icon text-2xl cursor-pointer ' />
                         <FaInstagram className='icon text-2xl cursor-pointer ' />
                         <FaTwitter className='icon text-2xl cursor-pointer ' />
                         <FaYoutube className='icon text-2xl cursor-pointer ' />
-                    </div> */}
+                    </div>
                 </div>
             </div>
             
