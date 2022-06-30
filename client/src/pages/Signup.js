d3f5f0b4c6887b73611ac27644b4d9236bf4afee
Sanjeeban2411 <sanju.sanjeeban.sp@gmail.com>
2022-06-30 20:59:33 +0530

    signup + axios

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,12 +1,29 @@
 import React, { useState } from 'react'
-
+import axios from 'axios'
 export default function Signup() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [data, setData] = useState("");
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(name)
+        setData( {
+            "name": name,
+            "email": email,
+            "password": password.toString
+        })
+        console.log(data)
+        axios({
+            method: 'post',
+            url: '/signup',
+            data: data
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
     // console.log(email)
     return (
@@ -18,21 +35,21 @@ export default function Signup() {
                     placeholder='Name'
                     onChange={(e) => { setName(e.target.value) }}
                     value={name}
-                    />
+                />
                 <input
                     type="text"
                     name='email'
                     placeholder='Email'
-                    onChange={(e) => { setEmail(e.target.value) }} 
+                    onChange={(e) => { setEmail(e.target.value) }}
                     value={email}
-                    />
+                />
                 <input
-                    type="text"
+                    type="password"
                     name='password'
                     placeholder='Password'
-                    onChange={(e) => { setPassword(e.target.value) }} 
+                    onChange={(e) => { setPassword(e.target.value) }}
                     value={password}
-                    />
+                />
                 <button onClick={handleSubmit}>SignUp</button>
             </form>
         </div>
