e6556b690a2648d4aaa44a361f6a61317a0ce7fa
unknown <dc3016@srmist.edu.in>
2022-09-08 17:50:26 +0530

    new Signup ui added

--- client/src/pages/LogUp.js
+++ client/src/pages/LogUp.js
@@ -38,21 +38,37 @@ const LogUp = () => {
 
   return (
     <>
-     <section className='flex flex-col md:flex-row h-screen items-center'>
-        <div className='h-screen hidden lg:block md:w-1/2 xl:w-2/3'>
-            <img className='w-full h-full object-cover' src="/assests/loginbg.jpg" alt=""/>
-        </div>
-        <div className='bg-white w-full md:max-w-md lg:max-w-full  md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
-          flex items-center justify-center'>
-            <div className='w-full h-100'>
-                <h1 className='text-3xl font-bold'>PAYMATE</h1>
-                <h1 className='text-xl md:text-2xl font-bold leading-tight mt-12'>Create Your Account</h1>
+    <div className="bg-[#DEF6F9] h-screen px-11 py-8">
+     <section className='flex flex-col md:flex-row h-full items-center  rounded-2xl bg-white'>
+        <div className='h-screen hidden lg:w-1/2 md:block xl:w-[60%]'>
+        <div className="flex flex-col">
+        <img
+              className="w-[60%] h-[40%] object-center flex items-center justify-center mt-40 mx-auto"
+              src="/assests/log-2.png"
+              alt=""
+            />
 
+        </div>
+        <div className="text-center text-[30px] font-extrabold">Split expenses with your <p>friends easily with our <spam className="text-[#02A9EA]">Rooms</spam></p></div>
+            <img src="/assests/indicator.png" alt="" className="mx-auto mt-8"/>
+            {/* <img className='w-full h-full object-cover' src="/assests/loginbg.jpg" alt=""/> */}
+        </div>
+        <div id="bg"
+            className=" w-full md:max-w-md lg:max-w-4xl md:mx-0 md:w-2/3 xl:w-[40%] h-[100%] px-2 lg:px-16 xl:px-28
+          flex items-center justify-center rounded-r-2xl">
+            <div className='w-full h-100 text-center'>
+            <div className="rounded-full flex items-center justify-center mt-7 bg-white mx-36  relative box-border min-w-[100px] min-h-[100px]">
+                <img src="/assests/logo.png" alt="" className="ml-3"/>
+              </div>
+              <h1 className="text-3xl font-bold mt-6">Hello There!</h1>
+              <h1 className="text-xl md:text-2xl font-bold leading-tight mt-4 text-white">
+                Welcome to Paymate
+              </h1>
                 <form action='submit' className='mt-6'>
                     
                     <div className='mt-4'>
-                        <label className='block text-gray-700'>Username</label>
-                        <input className=' w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none'
+                        {/* <label className='block text-gray-700'>Username</label> */}
+                        <input className=' w-full px-4 py-3 rounded-lg bg-white placeholder-[#40B3BE] mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none'
                             type="text"
                             name='username'
                             placeholder='Name'
@@ -69,8 +85,8 @@ const LogUp = () => {
                         />
                     </div>
                     <div className='mt-4'>
-                        <label className='block text-gray-700'>E-mail</label>
-                        <input className='w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none'
+                        {/* <label className='block text-gray-700'>E-mail</label> */}
+                        <input className='w-full px-4 py-3 rounded-lg bg-white placeholder-[#40B3BE] mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none'
                             type="text"
                             name='email'
                             placeholder='Email'
@@ -86,8 +102,8 @@ const LogUp = () => {
                         />
                     </div>
                     <div className='mt-4'>
-                        <label className='block text-gray-700'>Password</label>
-                        <input className='w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+                        {/* <label className='block text-gray-700'>Password</label> */}
+                        <input className='w-full px-4 py-3 rounded-lg bg-white placeholder-[#40B3BE] mt-2 border focus:border-blue-500
                   focus:bg-white focus:outline-none'
                             type="password"
                             name='password'
@@ -107,17 +123,17 @@ const LogUp = () => {
             <a href="/" className="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Forgot Password?</a>
           </div> */}
                     
-                    <button className='w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
+                    <button className='w-full block bg-[#2176AE] hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
                 px-4 py-3 mt-6' onClick={handleSubmit}  >SignUp</button>
                 </form>
                 <hr className='my-6 border-gray-300 w-full'/>
                 <p className="mt-8">
-              Already had an account?
+              Already have an Account?
               <a
                 href="/signin"
-                className="text-blue-500 hover:text-blue-700 font-semibold"
+                className="text-blue-500 hover:text-blue-700 font-semibold pl-1"
               >
-                Login to your account
+                Sign In
               </a>
             </p>
 
@@ -127,6 +143,7 @@ const LogUp = () => {
             </div>
         </div>
     </section> 
+    </div>
     </>
   )
 }
