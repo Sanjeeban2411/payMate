463b9e9b1d24eeee1cd282fd58185249f85b5f13
unknown <dc3016@srmist.edu.in>
2022-09-20 23:48:09 +0530

    rooms card designed

--- client/src/pages/Rooms.js
+++ client/src/pages/Rooms.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 import axios from 'axios'
 import Navbar from '../components/Navbar'
+import {BiShareAlt} from "react-icons/bi"
 
 const Rooms = (props) => {
   let navigate = useNavigate();
@@ -67,22 +68,22 @@ const Rooms = (props) => {
     <div>
       <Navbar />
       <div className=' absolute w-full h-[60%] top-50% flex flex-col mt-28 font-Montserrat text-3xl  text-black px-10'>
-        <div className='font-extrabold'>ROOMS</div>
-        <div className='mt-12 flex flex-row bg-black text-white m-auto rounded-lg p-2 hover:bg-gray-400 hover:text-black'><div><a href="/createroom">Create a Room</a></div>
-          {/* <div className='flex flex-col bg-gray-400'><div>Abode A306</div><div>5 Members</div> </div> */}
-        </div>
+        <div className=''>ROOMS</div></div>
+        <div className='mt-12  bg-black text-white m-auto rounded-lg p-2 hover:bg-gray-400 hover:text-black'><div><a href="/createroom">Create a Room</a></div>
+          
+        
         <div className='mt-8 bg-black text-white m-auto rounded-lg p-2 hover:bg-gray-400 hover:text-black'><a href="/joinroom">Join a Room</a></div>
         <div className='mt-12 mb-6 font-extrabold'>Your Rooms</div>
         <div className='grid grid-cols-2 md:grid-cols-4 gap-4 sm:grid-cols-3'>
           {/* {data.map(card)} */}
           {data.map((val) => {
             return (
-              <div className='flex flex-col bg-black  relative box-border items-center mx-auto rounded-full min-w-[200px] min-h-[200px] border-2 border-black '>
+              <div className='flex flex-col   relative box-border items-center mx-auto rounded-xl min-w-[300px] min-h-[300px] border-2 border-black ' id="bg-card">
                 <p className='relative text-center my-6 text-white font-extrabold'>{val.name}</p>
                 {/* <p className='relative text-center my-3 text-white font-extrabold'></p> */}
                 <button
-                  className='bg-white hover:bg-slate-500 hover:text-white text-black mx-auto p-2 my-3 rounded-md'
-                  onClick={event => enterRoom(event, val)}
+                  className='bg-white hover:bg-slate-500 hover:text-white text-black mx-auto p-2 my-3 rounded-md '
+                  onClick={event => enterRoom(event, val)} id="enter-bg"
                 >
                   {/* () => {
                    props.setRoom(val)
@@ -95,10 +96,10 @@ const Rooms = (props) => {
                 </button>
 
                 <button 
-                className='bg-white'
-                onClick={event => shareRoom(event, val)}
+                className=' bg-transparent hover:bg-slate-500 hover:text-white text-black mx-auto p-2 my-3 rounded-md'
+                onClick={event => shareRoom(event, val)} 
                 >
-                  Share
+                  <div className='flex flex-row'><h3>Share</h3><h3 className='p-1 '><BiShareAlt/></h3></div> 
                 </button>
               </div>
             )
