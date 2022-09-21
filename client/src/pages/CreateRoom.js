85a7e03c4f0912585c48619787b7336b83313bf6
unknown <dc3016@srmist.edu.in>
2022-09-22 00:57:28 +0530

    rooms create  ui/ux created

--- client/src/pages/CreateRoom.js
+++ client/src/pages/CreateRoom.js
@@ -45,43 +45,72 @@ const CreateRoom = () => {
 
 
     return (
-        <form action='submit' className='max-w-[400px] w-full mx-auto bg-white p-8'>
-                    <h2 className='text-4xl font-bold text-center py-8'>Create Room</h2>
-                    <div className='flex flex-col mb-4'>
-                        <label>Room</label>
-                        <input className=' border relative bg-gray-100 p-2'
-                            type="text"
-                            name='room'
-                            placeholder='Name'
-                            onChange={(e) => { 
-                                setName(e.target.value)
-                                // setData({
-                                //     "name": e.target.value,
-                                //     "password": data.password
-                                // });
-                             }}
-                            value={name}
-                        />
-                    </div>
-                    <div className='flex flex-col mb-4'>
-                        <label>Password</label>
-                        <input className=' border relative bg-gray-100 p-2'
-                            type="password"
-                            name='password'
-                            placeholder='Password'
-                            onChange={(e) => { 
-                                setPassword(e.target.value)
-                                // setData({
-                                //     "name": data.name,
-                                //     "password": e.target.value
-                                // });
-                             }}
-                            value={password}
-                        />
-                    </div>
-                    <button className='bg-[#E18A07] relative text-white font-extrabold text-xl w-full py-3 mt-8' onClick={handleSubmit}  >Create Room</button>
-                    {/* <p className='flex items-center mt-2'><input className='mr-2' type="checkbox" />Remember Me</p> */}
-                </form>
+        <>
+              <div className="bg-[#DEF6F9] h-screen px-11 py-8">
+        <section className="flex flex-col md:flex-row h-full items-center rounded-2xl bg-white">
+          <div className="h-screen hidden lg:w-1/2 md:block xl:w-[60%]">
+          <img
+          className="w-[80%] h-[60%] object-center flex items-center justify-center mt-16 mx-auto"
+          src="/assests/room-login.png"
+          alt="Second slide"
+        />
+            {/* <CarouselComp /> */}
+           
+            <div className="text-center text-[30px] font-extrabold w-1/2 mx-auto">Create rooms and add all your mates</div>
+            {/* <CarouselIndicater /> */}
+          </div>
+          <div 
+            className=" w-full md:max-w-md lg:max-w-4xl md:mx-0 md:w-2/3 xl:w-[40%] h-[100%] px-2 lg:px-16 xl:px-28
+          flex items-center justify-center rounded-r-2xl border-l-2 border-black"
+          >
+            <div className=" w-[90%] h-[90%] text-center border-[3px] border-[#2176AE]  rounded-[20px]">
+                <div className=' w-[90%] h-[55%] mx-auto my-32 '>
+              
+              <h1 className="text-3xl font-bold my-auto">Create Room</h1>
+              
+              <form action="submit" className="mt-10 px-2 ">
+                
+                <div className="">
+                  
+                  <input
+                    className="w-full px-4 py-2 rounded-[10px] bg-white border-2 border-[#2176AE] placeholder-[#2176AE] placeholder:text-center"
+                    type="text"
+                    name="room"
+                    placeholder="Name"
+
+                    onChange={(e) => { setName(e.target.value) }}
+                    value={name}
+                  />
+                </div>
+                <div className="my-4">
+                  <input
+                    className="w-full px-4 py-2 rounded-[10px] bg-white  border-2 border-[#2176AE] placeholder-[#2176AE] placeholder:text-center"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    value={password}
+                  />
+                </div>
+              
+                <button
+                  className="w-1/3 block font-extrabold bg-[#2176AE] hover:bg-blue-400 focus:bg-blue-400 text-white  rounded-lg
+                px-4 py-2 mt-5 mx-auto"
+                onClick={handleSubmit}
+                >
+                  Create
+                </button>
+              
+              </form>
+              </div>
+
+            </div>
+          </div>
+        </section>
+      </div>
+        </>
     );
 }
 
--- client/src/pages/JoinRoom.js
+++ client/src/pages/JoinRoom.js
@@ -166,42 +166,5 @@ export default function JoinRoom() {
         </section>
       </div>
     </>
-        // <div>
-        //     <form action='submit' className='max-w-[400px] w-full mx-auto bg-white p-8'>
-        //         <h2 className='text-4xl font-bold text-center py-8'>JOIN ROOM</h2>
-        //         <div className='flex flex-col mb-4'>
-        //             <label>Room</label>
-        //             <input className=' border relative bg-gray-100 p-2'
-        //                 type="text"
-        //                 name='room'
-        //                 placeholder='Name'
-        //                 onChange={(e) => { setName(e.target.value) }}
-        //                 value={name}
-        //             />
-        //         </div>
-        //         <div className='flex flex-col mb-4'>
-        //             <label>Password</label>
-        //             <input className=' border relative bg-gray-100 p-2'
-        //                 type="password"
-        //                 name='password'
-        //                 placeholder='Password'
-        //                 onChange={(e) => { setPassword(e.target.value) }}
-        //                 value={password}
-        //             />
-        //         </div>
-        //         <div className='flex flex-col mb-4'>
-        //             <label>Link</label>
-        //             <input className=' border relative bg-gray-100 p-2'
-        //                 type="text"
-        //                 name='link'
-        //                 placeholder='Join Link'
-        //                 onChange={(e) => { setLink(e.target.value) }}
-        //                 value={link}
-        //             />
-        //         </div>
-        //         <button className='bg-[#E18A07] relative text-white font-extrabold text-xl w-full py-3 mt-8' onClick={create}  >Join Room</button>
-        //         {/* <p className='flex items-center mt-2'><input className='mr-2' type="checkbox" />Remember Me</p> */}
-        //     </form>
-        // </div>
     )
 }
